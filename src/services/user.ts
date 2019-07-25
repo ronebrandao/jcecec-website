4611ef78e7cca5dd2066de45d6bc4e3f027c45bd
Rone Cesar <ronecesar@tilix.com.br>
2019-07-25 00:45:42 -0300

    make it better

---
+++ src/services/user.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import SignUpForm from '@/models/forms/SignUpForm';
+import { formatarData } from '@/helpers/index';
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+export async function createUser(user: SignUpForm): Promise<any> {
+
+    const {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        institution,
+        type,
+        cep,
+        street,
+        streetNumber,
+        state,
+        city,
+        neighborhood,
+        complement
+    } = user;
+
+    const birthDate = formatarData(user.birthDate);
+
+    const resp = await axios.post(API_URL + 'users', {
+        firstName,
+        lastName,
+        email,
+        birthDate,
+        phoneNumber,
+        institution,
+        type,
+        cep,
+        street,
+        streetNumber,
+        state,
+        city,
+        neighborhood,
+        complement
+    });
+
+    return resp.data;
+}
