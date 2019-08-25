0a8d46943becb2e51afd4734c8f2828084eabe95
Higor Ferreira <hfashigor@hotmail.com>
2019-08-25 18:08:33 -0300

    Adjust

--- src/components/organization/Main.vue
+++ src/components/organization/Main.vue
@@ -27,7 +27,7 @@ main {
   text-align: left;
   padding: 100px 14.6484375vw;
 }
-main > * h1, main > * h2, main > * h3, main > * h4, main > * h5, main > * h6 {
+main h1, main h2, main h3, main h4, main h5, main h6 {
   text-transform: uppercase;
   font-weight: 540;
   color: #2776f5;
@@ -36,7 +36,7 @@ main p {
   text-align: justify;
 }
 main > div:not(:last-child) {
-  margin-bottom: 15px;
+  margin-bottom: 45px;
 }
 
 </style>
