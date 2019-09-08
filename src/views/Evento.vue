81794c9ed20075a6d7affba838b45ecd14d49b17
Higor Ferreira <hfashigor@hotmail.com>
2019-09-08 18:15:27 -0300

    Inclusion of speakers n adjust on page text

--- src/views/Evento.vue
+++ src/views/Evento.vue
@@ -35,27 +35,33 @@
         primeiro e segundo grau
       </Paragraph>
 
-      <Paragraph title="II HACKATHON PUC GOIÁS">
-        Hackatons são eventos cada vez mais frequentes nas comunidades tecnológicas que promovem o trabalho em
-        equipe, divulgando os melhores alunos e oportunizando futuras vagas no mercado de trabalho. Estes eventos
-        têm sido promovidos comumente em grandes capitais brasileiras como São Paulo e Rio de Janeiro e têm
-        como objetivo resolver problemas ligados a tecnologia envolvendo empresas e instituições que desejem
-        apresentar uma demanda a ser solucionada. A Pontifícia Universidade Católica de Goiás deseja com este
-        evento oportunizar ao aluno a experiência de participar de hackatons como preparação para participação em
-        hackthons de âmbito nacional e internacional. A iniciativa de promover o I Hackathon da PUC GOIÁS no
-        âmbito da JCECEC tem o propósito de produzir a experiência visando disseminar a ideia no âmbito regional.
-      </Paragraph>
+      <Columns>
+        <Paragraph title="II HACKATHON PUC GOIÁS">
+          Hackatons são eventos cada vez mais frequentes nas comunidades tecnológicas que promovem o trabalho em
+          equipe, divulgando os melhores alunos e oportunizando futuras vagas no mercado de trabalho. Estes eventos
+          têm sido promovidos comumente em grandes capitais brasileiras como São Paulo e Rio de Janeiro e têm
+          como objetivo resolver problemas ligados a tecnologia envolvendo empresas e instituições que desejem
+          apresentar uma demanda a ser solucionada. A Pontifícia Universidade Católica de Goiás deseja com este
+          evento oportunizar ao aluno a experiência de participar de hackatons como preparação para participação em
+          hackthons de âmbito nacional e internacional. A iniciativa de promover o I Hackathon da PUC GOIÁS no
+          âmbito da JCECEC tem o propósito de produzir a experiência visando disseminar a ideia no âmbito regional.
+        </Paragraph>
 
-      <Paragraph title="I COMPETIÇÃO DE ROBÓTICA">
-        As competições de robôs reúnem construtores de robôs que competem para mostrar qual deles é
-        capaz de cumprir da melhor forma um determinado objetivo especificado anteriormente à competição. As
-        competições podem ser diferenciadas segundo alguns critérios básicos, a saber: propósitos científicos e
-        acadêmicos, grau de autonomia exigida dos protótipos de robôs para a realização das tarefas a eles
-        solicitadas, restrições para a construção do protótipo, ou seja, o quão livre é a escolha das plataformas
-        robóticas que podem ser empregadas na construção dos protótipos e Ua maneira como a organização da
-        equipe está vinculada a iniciativas de instituições educacionais ou organizações formais. A competição de
-        robôs da IV JCECEC tem vai explorar dois dos critérios propósitos científicos e acadêmicos, grau de
-        autonomia exigida dos protótipos de robôs para a realização das tarefas a eles solicitadas.
+        <Paragraph title="I COMPETIÇÃO DE ROBÓTICA">
+          As competições de robôs reúnem construtores de robôs que competem para mostrar qual deles é
+          capaz de cumprir da melhor forma um determinado objetivo especificado anteriormente à competição. As
+          competições podem ser diferenciadas segundo alguns critérios básicos, a saber: propósitos científicos e
+          acadêmicos, grau de autonomia exigida dos protótipos de robôs para a realização das tarefas a eles
+          solicitadas, restrições para a construção do protótipo, ou seja, o quão livre é a escolha das plataformas
+          robóticas que podem ser empregadas na construção dos protótipos e Ua maneira como a organização da
+          equipe está vinculada a iniciativas de instituições educacionais ou organizações formais. A competição de
+          robôs da IV JCECEC tem vai explorar dois dos critérios propósitos científicos e acadêmicos, grau de
+          autonomia exigida dos protótipos de robôs para a realização das tarefas a eles solicitadas.
+        </Paragraph>
+      </Columns>
+
+      <Paragraph title="palestrantes">
+        <Speakers :speakers="speakers" />
       </Paragraph>
     </Main>
   </div>
@@ -85,6 +91,43 @@ export default class Evento extends Vue {
 
   constructor() {
     super();
+
+    this.speakers = [
+      {
+        name: "CARLOS ALBERTO DE NOBREGA",
+        institution: "fessor at Hampden-Sydney College in Virginia, looked ",
+        speaking: "theory of ethics",
+        description:
+          "lievable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,",
+        img: "/assets/img/speakers/sp1.jpg",
+        action: () => {
+          alert("You clicked on the first speaker");
+        }
+      },
+      {
+        name: "CARLOS ALBERTO DE NOBREGA",
+        institution: "fessor at Hampden-Sydney College in Virginia, looked ",
+        speaking: "theory of ethics",
+        description:
+          "lievable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,",
+        img: "/assets/img/speakers/sp2.png",
+        action: () => {
+          alert("You clicked on the second speaker");
+        }
+      },
+      {
+        name: "CARLOS ALBERTO DE NOBREGA",
+        institution: "fessor at Hampden-Sydney College in Virginia, looked ",
+        speaking: "theory of ethics",
+        description:
+          "lievable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,",
+        img: "/assets/img/speakers/sp3.png",
+        offset: "-15px",
+        action: () => {
+          alert("You clicked on the third speaker");
+        }
+      }
+    ];
   }
 }
 </script>
