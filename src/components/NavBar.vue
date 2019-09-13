fe8c3a83001aa4d35604184b2daec284dba11871
Higor Ferreira <hfashigor@hotmail.com>
2019-09-12 21:52:58 -0300

    Navbar basic layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    
+  <div class="navbar-h">
+    <div class="nav-info">
+      <!-- <img src="assets/lampada.png" alt=""> -->
+      <div>
+        <span class="title">IV JCECEC</span><br>
+        <span>Jornada Científica da Escola de<br> Ciências Exatas e da Computação</span>
+      </div>
+      
+      <div class="nav-date">
+        <span>
+          De 6 a 9 de Novembro<br>
+          PUC Goiás<br>
+          Goiânia-GO
+        </span>
+      </div>
+    </div>
+    <div class="nav-container">
+      <ul>
+        <li>
+          <router-link to="/">HOME</router-link>
+        </li>
+        <li>
+          <router-link to="/evento">Evento</router-link>
+        </li>
+        <li>
+          <router-link to="/organizacao">Organização</router-link>
+        </li>
+        <li>
+          <router-link to="/cadastro">Inscrição</router-link>
+        </li>
+        <li>
+          <router-link to="/submissao">Submissão</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,4 +45,76 @@ export default class NavBar extends Vue {}
 </script>
 
 <style scoped >
+.navbar-h {
+  position: relative;
+  padding: 12px 0;
+  background-color: white;
+  margin: auto;
+}
+.navbar-h .nav-info {
+  color: #576574;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+.navbar-h .nav-info .title {
+  font-size: 30px;
+}
+.navbar-h .nav-info > div {
+  position: relative;
+}
+.navbar-h .nav-info > div:first-child {
+  text-align: left;
+}
+.navbar-h .nav-info > div:last-child {
+  text-align: right;
+}
+.navbar-h .nav-info span, .navbar-h .nav-info p, .navbar-h .nav-info h1, .navbar-h .nav-info h2, .navbar-h .nav-info h3 {
+  color: inherit;
+}
+.navbar-h .nav-date {
+  font-style: italic;
+  font-size: smaller;
+  display: flex;
+  align-items: flex-end;
+}
+.navbar-h .nav-container {
+  display: flex;
+  justify-content: center;
+}
+.navbar-h .nav-container > ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  border-top: 2px solid #576574;
+}
+.navbar-h .nav-container > ul li {
+  padding: 15px 45px;
+  font-family: "Montserrat", sans-serif;
+  color: #576574;
+  transition: all 300ms ease;
+}
+.navbar-h .nav-container > ul li:hover {
+  background-color: #576574;
+  color: white;
+  cursor: pointer;
+}
+.navbar-h .nav-container > ul li a {
+  color: inherit;
+}
+.navbar-h .nav-container > ul li a:link {
+  color: inherit;
+  text-decoration: none;
+}
+.navbar-h .nav-container > ul li a:active {
+  text-decoration: none;
+}
+.navbar-h .nav-container > ul li a:visited {
+  text-decoration: none;
+}
+.navbar-h .nav-container > ul li a:hover {
+  border: none;
+  text-decoration: none;
+}
 </style>
