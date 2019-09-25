d1a1866901b5206f34683ddc50a84ee595310c68
Higor Ferreira <hfashigor@hotmail.com>
2019-09-25 18:15:08 -0300

    Small adjust

--- src/components/miniCourse/index.vue
+++ src/components/miniCourse/index.vue
@@ -9,8 +9,8 @@
         
         <div class="info-h">
             <div>
-                <h5>Instrutor: {{course.instructor}}</h5>
-                <h6>Instituição: {{course.institution}}</h6>
+                <h5 :title="course.instructor">Instrutor: {{course.instructor}}</h5>
+                <h6 :title="course.institution">Instituição: {{course.institution}}</h6>
             </div>
             <div class="date-time-place">
                 <i class="far fa-calendar-alt"></i>
