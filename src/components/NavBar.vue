e78830b63bbd01de633d482c2ae69a2a583c5700
Rone <rone_filho@hotmail.com>
2019-10-24 11:29:34 -0300

    Adjust menu name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,9 +12,9 @@
       <ul>
         <router-link to="/" tag="li">Home</router-link>
         <router-link to="/evento" tag="li">Evento</router-link>
-        <router-link to="/jges" tag="li">Evento 2</router-link>
         <router-link to="/minicursos" tag="li">Minicursos</router-link>
         <router-link to="/competicao" tag="li">Competição</router-link>
+        <router-link to="/jges" tag="li">JGES</router-link>
         <router-link to="/cadastro" tag="li">Inscrição</router-link>
         <router-link to="/submissao" tag="li">Submissão</router-link>
       </ul>
@@ -58,6 +58,7 @@
         <router-link to="/evento" tag="li">Evento</router-link>
         <router-link to="/minicursos" tag="li">Minicursos</router-link>
         <router-link to="/competicao" tag="li">Competição</router-link>
+        <router-link to="/jges" tag="li">JGES</router-link>
         <router-link to="/cadastro" tag="li">Inscrição</router-link>
         <router-link to="/submissao" tag="li">Submissão</router-link>
       </ul>
